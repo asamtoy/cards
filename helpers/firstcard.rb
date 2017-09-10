4599c28d6c1d00500b7fd16e8e6f5d7e3daa27cd
Andrew Samtoy <asamtoy@gmail.com>
2017-09-10 14:00:33 +0100

    Headlines - updated

--- helpers/firstcard.rb
+++ helpers/firstcard.rb
@@ -1,9 +1,60 @@
 class FirstCard
 
   @@firstcard = [
-    'I never thought I would be jealous of',
-    'I wish I could cut',
+    'Tony Blair',
+    'Peter Kay',
+    'Hurricane Irma',
+    'Liam Gallagher',
+    'Lollapalooza',
+    'Endometriosis',
+    'stealth drinking',
+    'Netflix',
+    'Clare Teal',
+    'Sam Smith',
+    'Pink',
+    'Beyonce',
+    'Madonna',
+    'Asda',
+    'Andy Murray',
+    'Adele',
+    'Strictly Come Dancing',
+    'Tess Daly',
+    'Claudia Winkleman',
+    'Donald Trump',
+    'Kim Jong Un',
+    'Isis',
+    'Elon Musk',
+    'Theresa May',
+    'Steve Bannon',
+    'Conor McGregor',
+    'Italian cops',
+    'Saudi government',
+    'Prince Harry',
+    'Wayne Rooney',
+    'Vicious thug',
+    'Couple',
+    'Madeleine McCann\'s parents',
+    'Police',
+    'Young Winston Churchill',
+    'Prince Charles',
+    'British tourist',
+    'Doctor Foster',
+    'A dietician',
+    'Emma Thompson',
+    'Braless Charlize Theron',
+    'Hugh Grant',
+    'Mel B',
+    'Elizabeth Hurley',
+    'Emma Roberts',
+    'George Clooney',
+    'Justin Bieber',
+    'Ashlee Simpson',
+    'Benedict Cumberbatch',
+
+
+
   ]
+
 
   def self.random()
     random_firstcard = @@firstcard.sample
